541c850ca197dbc7100265b0db9f8d19e916e9c8
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-06-05 17:56:51 -0300

    Geração de itens aleatórios concluida

---
+++ programa.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+// constantes de controle
+const int N_ITEMS = 40;         // quantidade de itens
+const int SEED = 12453;         // seed da aleatorização
+const int MIN_VALUE = 1;        // valor mínimo de cada item
+const int MAX_VALUE = 50;       // valor máximo de cada item
+const int MIN_WEIGHT = 1;       // peso mínimo de cada item
+const int MAX_WEIGHT = 10;      // peso máximo de cada item
+const int BAG_CAPACITY = 100;   // capacidade da mochila
+
+int main() {
+
+    // declaração dos vetores
+    std::vector<int> item_ids(N_ITEMS);     // IDs dos items, será inicializado com números em ordem, só por questão de organização
+    std::vector<int> item_values(N_ITEMS);  // valores dos itens, será inicializado com números aleatórios
+    std::vector<int> item_weights(N_ITEMS); // pesos dos itens, será inicializado com números aleatórios
+
+    // gerador de números aleatórios com a SEED definida nas constantes
+    srand(SEED);
+
+    // header da apresentação dos valores
+    std::cout << "POSSÍVEIS ITENS:" << std::endl;
+    std::cout << "Item\tValor\tPeso" << std::endl;
+
+    // inicialização e apresentação dos vetores
+    for (int i = 0; i < N_ITEMS; i++) {                                     // percorre elemento por elemento
+
+        // inicialização dos vetores
+        item_ids[i] = i;                                                    // o ID vai de 0 a N_ITEMS - 1, só serve para organização
+        item_values[i] = (rand() + MIN_VALUE) % MAX_VALUE + MIN_VALUE;      // o valor é aleatório dentro do intervalo [MIN_VALUE, MAX_VALUE]
+        item_weights[i] = (rand() + MIN_WEIGHT) % MAX_WEIGHT + MIN_WEIGHT;  // o pes é aleatório dentro do intervalo [MIN_WEIGHT, MAX_WEIGHT]
+
+        // apresentação dos vetores
+        std::cout << item_ids[i] << "\t";           // mostra os IDs dos itens
+        std::cout << item_values[i] << "\t";        // mostra os valores dos itens
+        std::cout << item_weights[i] << std::endl;  // mostra os pesos dos itens
+    }
+}
